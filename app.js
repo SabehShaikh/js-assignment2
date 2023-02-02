de3fab9ac2cc05fa3e616f85455e0ac9fc490c80
SabehShaikh <121764329+SabehShaikh@users.noreply.github.com>
2023-02-02 15:28:13 +0500

    added html and javascript file.

---
+++ app.js
@@ -0,0 +1,116 @@
+// Chapter 2 
+
+// question 1
+
+var username;
+
+// question 2
+
+var myName = "Sabeh Shaikh"
+
+// question 3
+
+var message = "Hello World";
+alert(message);
+
+// question 4
+
+var name = "John Doe";
+var age = 15;
+var skills = "Certified Mobile Application Development";
+
+alert(name)
+alert(age)
+alert(skills)
+
+// question 5
+
+var name = "PIZZA \n PIZZ \n PIZ \n PI \n P"
+
+alert(name)
+
+// question 6 
+
+var email = "sabehshaikh201@gmail.com"
+
+alert(email)
+
+// question 7
+
+var book = " A smarter way to learn JavaScript"
+
+alert("I am trying to learn from the book" + book)
+
+// question 8
+
+document.write("<p> Yah! i can write HTML Content through JavaScript </p>")
+
+// question 9
+
+var name = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+
+alert(name)
+
+// chapter 3 VARIABLES FOR NUMBERS
+
+// question 1
+
+var age = 15
+alert(age)
+
+// question 2
+
+var visit = " 14 Times"
+alert("You have visited this website" + visit)
+
+// question 3 
+
+var birthYear = " 2004"
+document.write("My birth year is" + birthYear + "<br>");
+
+// question 4
+
+var visitorName = "John Doe";
+var productTitle = "T shirt";
+var quantity = 5;
+
+document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ clothing store");
+
+// chapter 4 VARIABLE NAMES: LEGAL & ILLEGAL
+
+// question 1
+
+var name = "Sabeh", age = "18", skill = "Web Developer";
+
+// question 2
+
+// legal variables 
+
+var rose;
+var football;
+var realMadrid;
+var barcelona;
+var arsenal;
+
+// illegal variables
+
+// var 1Rose;
+// var 2Football;
+// var Real Madrid;
+// var 4Barcelona;
+// var 5Arsenal;
+
+// question 3
+
+document.write("<h1> Rules for naming JS variables </h1>");
+
+document.write("<p> Variable names can only contain numbers, $, and ______. For example $my_1stVariable </p>");
+
+document.write("<p> Variables must begin with a letter, $ or _____. For example $name, _name or name </p>");
+
+document.write("<p> Variable names are case sensitive </p>")
+
+document.write("<p> Variable names should not be JS Keywords </p>")
+
+
+
